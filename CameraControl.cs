221bea984ea9405babd774f3ad29c2830cc7e062
Susanna Nurmi <susyy-@hotmail.com>
2014-02-14 11:23:18 +0200

    Player and CameControl
    working player movement, few bugs with ButtonInput function, i think

---
+++ CameraControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject capsule;
+    public GameObject playerCamera;
+    public float rotationSpeed = 1.0f;
+    private float maxY = 80.0f;
+    private float minY = -80.0f;
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        CameraRotation();
+	}
+
+    void CameraRotation()
+    {
+        if (Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse X") < 0)
+        {
+            capsule.transform.Rotate(new Vector3(0, 1, 0), rotationSpeed * Input.GetAxis("Mouse X"));
+        }
+        if (Input.GetAxis("Mouse Y") > 0 || Input.GetAxis("Mouse Y") < 0)
+        {
+            float rotationY = playerCamera.transform.localEulerAngles.x;
+            float tempRot = -rotationSpeed * Input.GetAxis("Mouse Y");
+
+            if(rotationY + tempRot < maxY)
+                playerCamera.transform.Rotate(new Vector3(1, 0, 0), -rotationSpeed * Input.GetAxis("Mouse Y"));
+            if(rotationY + tempRot > 360 + minY)
+                playerCamera.transform.Rotate(new Vector3(1, 0, 0), -rotationSpeed * Input.GetAxis("Mouse Y"));
+            if (rotationY < minY && rotationY < maxY)
+                playerCamera.transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    // character set up
+    private CharacterController controller;
+    private float gravity = 10.0f;
+    private float movementSpeed = 5.0f;
+    private float jumpingSpeed = 7.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start()
+    {
+        // also add a CharacterController to the player in Unity
+        controller = GetComponent<CharacterController>();
+
+        if(rigidbody)
+            rigidbody.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        Movement();
+        ButtonInput();
+	}
+
+    void Movement()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * movementSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+                moveDirection.y = jumpingSpeed;
+        }
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void ButtonInput()
+    {
+        if(Input.GetButtonDown("Run"))
+            movementSpeed = 9.5f;
+        else if(Input.GetButtonUp("Run"))
+            movementSpeed = 5.0f;
+        else if(Input.GetButtonDown("Sneak"))
+            movementSpeed = 2.0f;
+        else if(Input.GetButtonUp("Sneak"))
+            movementSpeed = 5.0f;
+    }
+}
